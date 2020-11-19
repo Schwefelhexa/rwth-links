a09575144a9833b4810ceea2e39950d14bf7557d
Alexander Baron <alexander@baron-online.eu>
2020-11-19 11:08:47 +0100

    No code splitting Phosphor 1.0.1 supports code splitting

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
 
-import ClientSide from '../util/ClientSide';
 import SEO from './SEO';
-const LinkBar = React.lazy(() => import('./LinkBar'));
+import LinkBar from './LinkBar';
 
 interface Props {
   title: string;
@@ -14,11 +13,7 @@ const Layout: React.FC<Props> = ({ context, title, children }) => {
   return (
     <div className="w-full min-h-screen relative overflow-hidden z-0">
       <SEO title={title} />
-      <ClientSide>
-        <React.Suspense fallback={<div />}>
-          <LinkBar context={context} />
-        </React.Suspense>
-      </ClientSide>
+      <LinkBar context={context} />
       <div className="w-full h-full px-6 py-6 lg:px-10 lg:py-10 z-10">{children}</div>
     </div>
   );
